eebc27ed03b814d172fec4188c5318dfc66cf3a3
Arman Mondal <admin@armanmondal.me>
2025-01-17 19:23:39 +0530

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -82,7 +82,7 @@ export default function Footer() {
           <div className="flex flex-col justify-between items-center lg:items-end lg:justify-self-end lg:col-span-4">
             <button 
             onClick={()=>{
-            window.location.href = 'https://privepa-merchant-web.vercel.app/';
+            window.location.href = 'https://merchant.privepay.in/';
             }}
 
             className="bg-[#ff5733] px-7 py-3 rounded-full text-neutral-white text-xs bg-gradient-to-r from-[#ff5733] to-[#ff5733] hover:button-brightness mb-7 focus:outline-none focus:ring ring-green-400">
