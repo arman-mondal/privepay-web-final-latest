409cf7457f5db604f847332669ec62e6a43e6f30
Arman Mondal <admin@armanmondal.me>
2025-02-11 18:35:41 +0530

    Update Privacy.tsx

--- src/components/terms/Privacy.tsx
+++ src/components/terms/Privacy.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Privacy() {
         return (
            <div className="content-container relative px-[20%] min-h-[100vh] mt-[10%]">
-    <p className="content-head">Terms &amp; Conditions</p>
+    <p className="content-head">Privacy Policy</p>
     <div className="content-seprater"></div>
     <p className="updated-date">Last updated on Aug 7th 2024</p>
     <p className="content-text">
