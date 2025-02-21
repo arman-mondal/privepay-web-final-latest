94e2d21b53c452bf26da85453ff9bd5d4bb49020
Arman Mondal <admin@armanmondal.me>
2025-02-21 13:13:09 +0530

    Update Motivation.tsx

--- src/components/home/Motivation.tsx
+++ src/components/home/Motivation.tsx
@@ -37,8 +37,7 @@ export default function Motivation() {
               Why choose PrivePay?
             </h2>
             <p className="text-neutral-grayish-blue text-sm lg:text-base leading-5">
-              We leverage Open Banking to turn your bank account into your
-              financial hub. Control your finances like never before.
+              Earn Rewards on spending at your favourite clubs and cafes with amazing discounts.
             </p>
           </div>
         </div>
